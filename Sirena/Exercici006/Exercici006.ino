74f6e9c0509283bc5668a6e8fe87002852efaa7b
DavidCasas2021 <david.casas.70@lacetania.cat>
2022-01-24 10:05:43 +0100

    OK

---
+++ Sirena/Exercici006/Exercici006.ino
@@ -0,0 +1,38 @@
+/****************************************************************
+ **                                                            **
+ **                           SIRENA                           **
+ **                                                            **
+ **                         Exercici 6                         **
+ **                                                            **
+ **  NOM: David Casas                        DATA: 23/01/2022  **
+ ***************************************************************/
+ //**************************INCLUDE*****************************
+
+
+ //*************************VARIABLES****************************
+const byte xiulet = 9;        // donar nom al pin 9 de l’Arduino
+const byte pot0 = A0;        // donar nom al pin A0 de l’Arduino
+const byte pot1 = A1;        // donar nom al pin A0 de l’Arduino
+int valPot0;                 // guardar valor del potenciometre 
+int valPot1;                 // guardar valor del potenciometre 
+
+ //***************************SETUP******************************
+
+void setup() {             // configura el final de salida 
+pinMode (xiulet, OUTPUT);  // definir el pin 9 com una sortida
+pinMode (pot0, INPUT);
+pinMode (pot1, INPUT);
+
+}
+ //***************************LOOP*******************************
+ 
+void loop() 
+{             // incia el bucle del programa
+  valPot0 = analogRead(pot0);
+  valPot1 = analogRead(pot1);   
+
+  tone(xiulet, valPot1, valPot0);
+  delay(2*valPot0);
+
+}
+ //*************************FUNCIONS*****************************
